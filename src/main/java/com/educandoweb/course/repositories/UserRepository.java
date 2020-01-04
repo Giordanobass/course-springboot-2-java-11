ed79d34dc74923a9e7536c440337958b70c81c7d
Giordanobass <giordanoamaral@gmail.com>
2020-01-04 13:42:48 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -6,5 +6,5 @@ import com.educandoweb.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	//É o suficiente para usar vários objetos e prontos para usar
-
+	//não é necessário a anotação @repository
 }
